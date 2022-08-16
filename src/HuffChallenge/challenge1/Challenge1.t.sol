49798ae787e1b4f68c5f292054b4a2c0f48259c7
minami <minaminaoy@gmail.com>
2022-08-17 07:09:48 +0900

    Add Huff challenge1

---
+++ src/HuffChallenge/challenge1/Challenge1.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.15;
+
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+contract Challenge1Test is Test {
+    function test() public {
+        // 6 bytes
+        Challenge challenge = Challenge(HuffDeployer.deploy("HuffChallenge/challenge1/Challenge1"));
+        emit log_uint(address(challenge).code.length);
+
+        uint32 blockNumber = challenge.blockNumber();
+
+        assertEq(blockNumber, block.number);
+    }
+}
+
+interface Challenge {
+    function blockNumber() external returns (uint32);
+}
