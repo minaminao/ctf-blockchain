1987dab056fa4915f68bd29b33a6882e5ec05304
minaminao <minaminaoy@gmail.com>
2022-08-18 09:55:52 +0900

    Add 25 gas solution

--- src/HuffChallenge/challenge2/Challenge2.t.sol
+++ src/HuffChallenge/challenge2/Challenge2.t.sol
@@ -5,19 +5,29 @@ import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 
 contract Challenge2Test is Test {
-    function test() public {
-        address solverAddress1 = HuffDeployer.deploy("HuffChallenge/challenge2/Solver26Gas");
-        emit log_uint(solverAddress1.code.length); // 11 bytes
-        validSolver(solverAddress1);
-        address solverAddress2 = HuffDeployer.deploy("HuffChallenge/challenge2/Solver28Gas");
-        emit log_uint(solverAddress2.code.length); // 11 bytes
-        validSolver(solverAddress2);
+    function testSolver28Gas() public {
+        address solverAddress = HuffDeployer.deploy("HuffChallenge/challenge2/Solver28Gas");
+        emit log_uint(solverAddress.code.length); // 11 bytes
+        validSolver(solverAddress);
+    }
+
+    function testSolver26Gas() public {
+        address solverAddress = HuffDeployer.deploy("HuffChallenge/challenge2/Solver26Gas");
+        emit log_uint(solverAddress.code.length); // 11 bytes
+        validSolver(solverAddress);
+    }
+
+    function testSolver25Gas() public {
+        vm.chainId(1); // mainnet
+        address solverAddress = HuffDeployer.deploy("HuffChallenge/challenge2/Solver25Gas");
+        emit log_uint(solverAddress.code.length); // 10 bytes
+        validSolver(solverAddress);
     }
 
     function validSolver(address solverAddress) public {
-        for (uint256 i = 0; i < 4; i++) {
+        for (uint256 i = 0; i < 10; i++) {
             (bool success, bytes memory data) = solverAddress.call(abi.encode(i));
-            require(success);
+            assertTrue(success);
             uint256 isEven = abi.decode(data, (uint256));
             assertEq(isEven, uint256((i + 1) % 2));
         }
