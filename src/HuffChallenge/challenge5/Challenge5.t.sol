e5b39a2d0f60c0b1ac5a7526c06af33a842a571a
minaminao <minaminaoy@gmail.com>
2022-11-03 19:45:03 +0900

    chore: change RPC

--- src/HuffChallenge/challenge5/Challenge5.t.sol
+++ src/HuffChallenge/challenge5/Challenge5.t.sol
@@ -10,7 +10,7 @@ contract Challenge5Test is Test {
 
     function setUp() public {
         // Fork mainnet
-        vm.createSelectFork("https://rpc.flashbots.net/");
+        vm.createSelectFork(vm.envString("RPC_ANKR_MAINNET"));
         solver = HuffDeployer.config().deploy("HuffChallenge/challenge5/Challenge5");
     }
 
