17c5e6acc6ca5eede67cc11c930fa9b36193afa5
Pranesh A S <42379522+PraneshASP@users.noreply.github.com>
2023-02-07 11:26:50 +0530

    Add solution for QuillCTF 2022 - Challenge 3 (#7)
    Co-authored-by: minaminao <minaminaoy@gmail.com>

--- src/QuillCTF2022/README.md
+++ src/QuillCTF2022/README.md
@@ -7,6 +7,7 @@ Here's the link to the challenges page: https://quillctf.super.site/challenges. 
 **Table of Contents**
 - [QuillHash CTF 2022 Solutions](#quillhash-ctf-2022-solutions)
   - [Road closed:](#road-closed)
+  - [VIP Bank:](#vip-bank)
   - [Confidential Hash:](#confidential-hash)
 
 
@@ -23,6 +24,19 @@ Goerli link: https://goerli.etherscan.io/address/0xd2372eb76c559586be0745914e953
  
 ```
 forge test --match-contract QuillCTF1Solved -vvvv
+```
+
+
+## VIP Bank:
+https://quillctf.super.site/challenges/quillctf-challenges/vip-bank
+
+**Objective:**
+At any cost, lock the VIP user balance forever into the contract.
+
+Goerli Link: https://goerli.etherscan.io/address/0x28e42e7c4bda7c0381da503240f2e54c70226be2
+
+```
+forge test --match-contract QuillCTF3Solved -vvvv
 ```
 
 ## Confidential Hash:
---
+++ src/QuillCTF2022/VIPBank/challenge/VIPBank.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract VIPBank {
+    address public manager;
+    mapping(address => uint256) public balances;
+    mapping(address => bool) public VIP;
+    uint256 public maxETH = 0.5 ether;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "you are not manager");
+        _;
+    }
+
+    modifier onlyVIP() {
+        require(VIP[msg.sender] == true, "you are not our VIP customer");
+        _;
+    }
+
+    function addVIP(address addr) public onlyManager {
+        VIP[addr] = true;
+    }
+
+    function deposit() public payable onlyVIP {
+        require(
+            msg.value <= 0.05 ether,
+            "Cannot deposit more than 0.05 ETH per transaction"
+        );
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 _amount) public onlyVIP {
+        require(
+            address(this).balance <= maxETH,
+            "Cannot withdraw more than 0.5 ETH per transaction"
+        );
+        require(balances[msg.sender] >= _amount, "Not enough ether");
+        balances[msg.sender] -= _amount;
+        (bool success, ) = payable(msg.sender).call{value: _amount}("");
+        require(success, "Withdraw Failed!");
+    }
+
+    function contractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
