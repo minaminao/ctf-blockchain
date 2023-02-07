347258e9b31588e78f06d35daffaae7511fc4b5e
minaminao <minaminaoy@gmail.com>
2023-02-08 00:50:30 +0900

    fix: application panic

--- src/HuffChallenge/challenge4/Challenge4.t.sol
+++ src/HuffChallenge/challenge4/Challenge4.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-only
-pragma solidity 0.8.15;
+pragma solidity ^0.8.15;
 
 import {Test} from "forge-std/Test.sol";
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
