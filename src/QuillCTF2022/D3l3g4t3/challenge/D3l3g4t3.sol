d56aa6f0b5cca25b1d60c619251fec60cc6963dc
PraneshASP <praneshas2000@gmail.com>
2023-02-16 16:32:30 +0700

    add d3l3g4t3 challenge contract

---
+++ src/QuillCTF2022/D3l3g4t3/challenge/D3l3g4t3.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract D31eg4t3{
+
+
+    uint a = 12345;
+    uint8 b = 32;
+    string private d; 
+    uint32 private c; 
+    string private mot;
+    address public owner;
+    mapping (address => bool) public canYouHackMe;
+
+    modifier onlyOwner{
+        require(false, "Not a Owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function hackMe(bytes calldata bites) public returns(bool, bytes memory) {
+        (bool r, bytes memory msge) = address(msg.sender).delegatecall(bites);
+        return (r, msge);
+    }
+
+
+    function hacked() public onlyOwner{
+        canYouHackMe[msg.sender] = true;
+    }
+}
