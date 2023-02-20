8015e71b00940282a22353a3f223a98c20f4fbef
PraneshASP <praneshas2000@gmail.com>
2023-02-20 15:45:27 +0700

    add poc for collatz puzzle

---
+++ src/QuillCTF2022/CollatzPuzzle/CollatzPuzzleSolved.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import {CollatzPuzzle} from "./challenge/CollatzPuzzle.sol";
+
+interface ICollatzPuzzle {
+    function collatzIteration(uint256 n) external pure returns (uint256);
+}
+
+contract CollatzPuzzleSolved is Test {
+    CollatzPuzzle public puzzle;
+    ICollatzPuzzle public solution;
+
+    /// @dev Setup the testing environment.
+    function setUp() public {
+        puzzle = new CollatzPuzzle();
+        solution = ICollatzPuzzle(
+            HuffDeployer.deploy(
+                "/QuillCTF2022/CollatzPuzzle/CollatzPuzzleSolution"
+            )
+        );
+    }
+
+    function testSolution() public {
+        assertEq(puzzle.callMe(address(solution)), true);
+    }
+}
